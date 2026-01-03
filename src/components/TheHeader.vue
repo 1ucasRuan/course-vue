184368a579a31193c34659129736bdda0f27b047
1ucasRuan <lucasruan1.tuc@gmail.com>
2026-01-03 20:30:48 -0300

    doc: add componete Header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="header">
+        header
+    </div>
+</template>
+
+<script>
+export default {
+    // name: 'TheHeader',
+};
+</script>
+
+<style>
+.header {
+    font-size: 24px;
+    margin-bottom: 20px;
+    color: blueviolet;
+}
+</style>
